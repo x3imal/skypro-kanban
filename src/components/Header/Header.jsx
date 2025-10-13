d7e48469aa66828702b95de3d6367c5a46a91c5c
alexeyasurov <aasurov@gmail.com>
2025-10-13 11:59:22 +0300

    hw2: добавили модалку, открывается и закрывается по клику. Отредактировал отображение и стили

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,37 @@
+import {useState} from "react";
 import PopUser from "../PopUser/PopUser.jsx";
 
 export default function Header() {
+    const [isUserOpen, setIsUserOpen] = useState(false);
+
+    const toggleUser = () => {
+        setIsUserOpen((prev) => !prev);
+    };
+
     return (
         <header className="header">
             <div className="container">
                 <div className="header__block">
                     <div className="header__logo _show _light">
-                        <a href="" target="_self"><img src="/logo.png" alt="logo" /></a>
+                        <a href="#"><img src="/logo.png" alt="logo"/></a>
                     </div>
                     <div className="header__logo _dark">
-                        <a href="" target="_self"><img src="/logo_dark.png" alt="logo" /></a>
+                        <a href="#"><img src="/logo_dark.png" alt="logo"/></a>
                     </div>
 
                     <nav className="header__nav">
-                        <button className="header__btn-main-new _hover01" id="btnMainNew">
-                            <a href="#popNewCard">Создать новую задачу</a>
-                        </button>
+                        <a href="#popNewCard" className="header__btn-main-new _hover01">
+                            Создать новую задачу
+                        </a>
 
-                        <a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
-                        <PopUser />
+                        <a href="#" className="header__user _hover02"
+                           onClick={(e) => { e.preventDefault(); toggleUser(); }}
+                            aria-expanded={isUserOpen}
+                        >
+                            Ivan Ivanov
+                        </a>
+
+                        <PopUser isOpen={isUserOpen} onClose={() => setIsUserOpen(false)}/>
                     </nav>
                 </div>
             </div>
